6d1e38be45695824ff6923be386acabd7c37e70c
Arjun.O <arjundlm10@gmail.com>
2026-01-12 21:53:39 +0530

    fixed double socket server creation error

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-
-const io = new Server(httpServer, {
-  cors: { origin: "*" },
-});
-
-io.on("connection", (socket) => {
-  console.log(`connected ${socket.id}`);
-});
 
 export { app, httpServer };
